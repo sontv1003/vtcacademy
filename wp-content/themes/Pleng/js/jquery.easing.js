1f9479fb6fb6d6856aacb69b3c6c5c940de83a1f
transontt <transontt@gmail.com>
2011-12-20 13:50:07 +0000

    Fix loi COntact form 7 bi dung voi Slide

--- wp-content/themes/Pleng/js/jquery.easing.js
+++ wp-content/themes/Pleng/js/jquery.easing.js
@@ -36,8 +36,8 @@
 */
 
 // t: current time, b: begInnIng value, c: change In value, d: duration
+$(document).ready(function() {
 jQuery.easing['jswing'] = jQuery.easing['swing'];
-
 jQuery.extend( jQuery.easing,
 {
 	def: 'easeOutQuad',
@@ -170,7 +170,7 @@ jQuery.extend( jQuery.easing,
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
 	}
 });
-
+});
 /*
  *
  * TERMS OF USE - EASING EQUATIONS
